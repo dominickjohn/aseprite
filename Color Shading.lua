949f3f2aadf52fb661624a1bf24af850b79077f7
Dominick John <42475437+dominickjohn@users.noreply.github.com>
2020-10-29 11:44:05 -0400

    Added right and middle mouse clicks
    -- Commands:
    --    "Get" Button: Regenerate color shades based on current foreground color.
    --    Left click: Set clicked color as foreground color
    --    Right click: Set clicked color as background color
    --    Middle click: Regenerate all shades based on clicked color
    --    "Base Color" picker: Non-functional (reference only)

--- Color Shading.lua
+++ Color Shading.lua
@@ -1,12 +1,24 @@
--- Aseprite Script to open dialog with relevant color shades
--- Written by Dominick John, 2019
--- https://github.com/alpineboarder/aseprite/
+-- Aseprite Script to open palette picker window with relevant color shades
+-- Written by Dominick John, @dominickjohn
+-- Contributed to by David Capello
+-- https://github.com/dominickjohn/aseprite/
+
+-- Instructions:
+--    Place this file into the Aseprite scripts folder (File -> Scripts -> Open Scripts Folder)
+--    Run the "Color Shading" script (File -> Scripts -> Color Shading) to open the palette window.
+
+-- Commands:
+--    "Get" Button: Regenerate color shades based on current foreground color.
+--    Left click: Set clicked color as foreground color
+--    Right click: Set clicked color as background color
+--    Middle click: Regenerate all shades based on clicked color
+--    "Base Color" picker: Non-functional (reference only)
 
 function lerp(first, second, by)
   return first * (1 - by) + second * by
 end
 
-function lerpRGBInt (color1, color2, amount)
+function lerpRGBInt(color1, color2, amount)
   local X1 = 1 - amount
   local X2 = color1 >> 24 & 255
   local X3 = color1 >> 16 & 255
@@ -34,32 +46,26 @@ function colorShift(color, hueShift, satShift, lightShift, shadeShift)
   newColor.hue = newColor.hue + hueShift * 359
 
   -- SHIFT SATURATION
-  if (satShift > 0)
-  then
+  if (satShift > 0) then
     newColor.saturation = lerp(newColor.saturation, 1, satShift)
-  elseif (satShift < 0)
-  then
+  elseif (satShift < 0) then
     newColor.saturation = lerp(newColor.saturation, 0, -satShift)
   end
 
   -- SHIFT LIGHTNESS
-  if (lightShift > 0)
-  then
+  if (lightShift > 0) then
     newColor.lightness = lerp(newColor.lightness, 1, lightShift)
-  elseif (lightShift < 0)
-  then
+  elseif (lightShift < 0) then
     newColor.lightness = lerp(newColor.lightness, 0, -lightShift)
   end
 
   -- SHIFT SHADING
-  local newShade = Color{red = newColor.red, green = newColor.green, blue = newColor.blue}
+  local newShade = Color {red = newColor.red, green = newColor.green, blue = newColor.blue}
   local shadeInt = 0
-  if (shadeShift >= 0)
-  then
+  if (shadeShift >= 0) then
     newShade.hue = 50
     shadeInt = lerpRGBInt(colorToInt(newColor), colorToInt(newShade), shadeShift)
-  elseif (shadeShift < 0)
-  then
+  elseif (shadeShift < 0) then
     newShade.hue = 215
     shadeInt = lerpRGBInt(colorToInt(newColor), colorToInt(newShade), -shadeShift)
   end
@@ -72,9 +78,10 @@ end
 
 function showColors()
   local dlg
-  dlg = Dialog{
-    title="Color Shading",
-    onclose=function()
+  dlg =
+    Dialog {
+    title = "Color Shading",
+    onclose = function()
       ColorShadingWindowBounds = dlg.bounds
     end
   }
@@ -115,36 +122,83 @@ function showColors()
   local H7 = colorShift(C, 0.15, 0, 0, 0)
 
   -- DIALOGUE
-  dlg
-  :color{ label="Current Color", color=C }
-  :button{ text="Get",
-           onclick=function()
-             dlg:close()
-             showColors()
-           end }
+  dlg:
+  color {
+    label = "Base",
+    color = C
+  }:button {
+    text = "Get",
+    onclick = function()
+      dlg:close()
+      showColors()
+    end
+  }:shades {
+     -- SHADING
+    id = "sha",
+    label = "Shade",
+    colors = {S1, S2, S3, C, S5, S6, S7},
+    onclick = function(ev)
+      if(ev.button == 1) then
+        app.fgColor = ev.color
+      elseif(ev.button == 2) then
+        app.bgColor = ev.color
+      elseif(ev.button == 3) then
+        app.fgColor = ev.color
+        dlg:close()
+        showColors()
+      end
+    end
+  }:shades {
+     -- LIGHTNESS
+    id = "lit",
+    label = "Light",
+    colors = {L1, L2, L3, C, L5, L6, L7},
+    onclick = function(ev)
+      if(ev.button == 1) then
+        app.fgColor = ev.color
+      elseif(ev.button == 2) then
+        app.bgColor = ev.color
+      elseif(ev.button == 3) then
+        app.fgColor = ev.color
+        dlg:close()
+        showColors()
+      end
+    end
+  }:shades {
+     -- SATURATION
+    id = "sat",
+    label = "Sat",
+    colors = {C1, C2, C3, C, C5, C6, C7},
+    onclick = function(ev)
+      if(ev.button == 1) then
+        app.fgColor = ev.color
+      elseif(ev.button == 2) then
+        app.bgColor = ev.color
+      elseif(ev.button == 3) then
+        app.fgColor = ev.color
+        dlg:close()
+        showColors()
+      end
+    end
+  }:shades {
+     -- HUE
+    id = "hue",
+    label = "Hue",
+    colors = {H1, H2, H3, C, H5, H6, H7},
+    onclick = function(ev)
+      if(ev.button == 1) then
+        app.fgColor = ev.color
+      elseif(ev.button == 2) then
+        app.bgColor = ev.color
+      elseif(ev.button == 3) then
+        app.fgColor = ev.color
+        dlg:close()
+        showColors()
+      end
+    end
+  }
 
-  -- SHADING
-  :shades{ id='sha', label='Shading',
-           colors={ S1, S2, S3, C, S5, S6, S7 },
-           onclick=function(ev) app.fgColor=ev.color end }
-
-  -- LIGHTNESS
-  :shades{ id='lit', label='Lightness',
-           colors={ L1, L2, L3, C, L5, L6, L7 },
-           onclick=function(ev) app.fgColor=ev.color end }
-
-  -- SATURATION
-  :shades{ id='sat', label='Saturation',
-           colors={ C1, C2, C3, C, C5, C6, C7 },
-           onclick=function(ev) app.fgColor=ev.color end }
-
-  -- HUE
-  :shades{ id='hue', label='Hue',
-           colors={ H1, H2, H3, C, H5, H6, H7 },
-           onclick=function(ev) app.fgColor=ev.color end }
-
-  dlg:show{ wait=false, bounds=ColorShadingWindowBounds }
-
+  dlg:show {wait = false, bounds = ColorShadingWindowBounds}
 end
 
 -- Run the script
